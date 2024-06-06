0b79554faf4bdf5e9bc6d68aa131a993b2250d43
Fadika08 <muchammadfadika@gmail.com>
2024-06-06 17:43:58 +0700

    Firts

---
+++ LoginPage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <content>
+        <div class="sidetext">
+            <div class="bigtext">
+    <div id="bigtext1">Welcome</div>
+    <div id="bigtext2">Back!</div>
+    </div>
+    <div class="smalltext"><div id="t1">Nostalgia memanggil! Login ke CemilanTempoDoeloe </div>
+                        <div id="t2">& jelajahi jajanan legendaris Nusantara. Dukung </div>
+                        <div id="t3">UMKM lokal & nikmati cita rasa masa kecil </div>
+                        <div id="t4">langsung diantar ke rumah!</div></div>
+        </div>
+        <div class="right-side">
+    <div id="logintext">Login</div>
+    <form class="login">
+        <label for="email">Email</label>
+        <input type="email" name="email" placeholder="Email">
+        <label for="Password">Password</label>
+        <input type="password" name="password" placeholder="Password">
+        <div id="remebmberme-box">
+        <input type="checkbox" id="rememberme" name="rememberme">
+        <label for="rememberme">Remember Me</label></div>
+        <button type="submit" class="but-login">Login</button>
+        <div id="lupa-password">
+                <a href="#">Lupa Password?</a>
+        </div>
+    </form>
+    <footer>
+    <div id="footer-text">
+        <a href="#">Terms of Service</a>
+        <div>|</div>
+        <a href="#">Privacy Policy</a>
+    </div>
+</footer>
+    </div>
+    </content>
+
+</body>
+</html>
---
+++ firstpage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CemilanTempoDoeloe</title>
+    <link rel="stylesheet" href="firstpage.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <ul>
+                <li class="nav-item"><a href="#">Register</a></li>
+                <li class="nav-item"> | </li>
+                <li class="nav-item"><a href="LoginPage.php" class="Login">Login</a></li>
+            </ul>               
+        </nav>
+    </header>
+    <content>
+        <div id="isi">
+        <div id="TeksBig">Temukan Jajanan Impianmu!</div>
+        <div id="TeksSmall">CemilanTempoDoeloe, Solusi Tepat untuk Ngidam Jajanan Tradisional.</div>
+        <div id="search-container">
+            <input type="text" name="Cari" placeholder="Cari">
+            <button id="search-button" type="submit" onclick="searchFunction()">
+      <img src="search.png" alt="Search">
+    </button>
+        </div>
+    </content>
+
+    <script>
+    window.addEventListener('load', function() {
+        var searchInput = document.getElementById('search-input');
+        var searchIcon = document.getElementById('search-icon');
+        var searchContainer = document.getElementById('search-container');
+        searchInput.style.width = (searchContainer.offsetWidth - searchIcon.offsetWidth) + 'px';
+        function searchFunction() {
+        var searchTerm = document.getElementById('search-input').value;
+        console.log('Searching for:', searchTerm);
+        }
+
+    });
+</script>
+</body>
+</html>
