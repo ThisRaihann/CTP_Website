ddf2afbec0d2a9b28ebf8267c80f21804ec1d779
ThisRaihann <mr.fewsx212@gmail.com>
2024-06-17 21:22:02 +0700

    min

--- public/dasboard_admin/index.php
+++ public/dasboard_admin/index.php
@@ -240,21 +240,6 @@ $jumlahAkun = $row['jumlah_akun'];
           <!-- ./col -->
           <div class="col-lg-3 col-6">
             <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
-
-                <p>Bounce Rate</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-stats-bars"></i>
-              </div>
-              <a href="#" class="small-box-footer">Cemilan Tempo Doeloe </a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
             <div class="small-box bg-warning">
               <div class="inner">
                 <h3><?php echo $jumlahAkun; ?></h3>
@@ -265,21 +250,6 @@ $jumlahAkun = $row['jumlah_akun'];
                 <i class="ion ion-person-add"></i>
               </div>
               <a href="#" class="small-box-footer">Cemilan Tempo Doeloe </a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h3>65</h3>
-
-                <p>Unique Visitors</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-pie-graph"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
